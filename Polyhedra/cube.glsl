edf906468ea6c083139882efab272fd3299bd78c
SiegfriedFabian <landerfabian@gmail.com>
2022-07-08 14:52:59 +0200

    Finished cube polyhedra

--- Polyhedra/cube.glsl
+++ Polyhedra/cube.glsl
@@ -10,6 +10,7 @@ uniform float u_dx;
 
 
 const float eps = 0.0001;
+const float n_eps = 1000.;
 float tMax = 50.;
 float b =2.0;
 float wall_height = 5. ;
@@ -132,10 +133,15 @@ float sdfGreen(vec3 p){
 }
 
 float sdfOrange(vec3 p){
-    float df = sdBox(vec3(p.xy, abs(p.z))- vec3(0. ,0., -3.*b ), vec3(b, wall_height, wall_thickness )); // Edge 4 & 12
-    df = opUnion(df, sdBox(vec3(p.xy, abs(p.z))- vec3(4.*b,0., b ), vec3(b, wall_height, wall_thickness ))); // Edge 7 & 9
+    float df = sdBox(vec3(p.xy, abs(p.z))- vec3(0. ,0., 3.*b ), vec3(b, wall_height, wall_thickness )); // Edge 4 & 12
     return df;
 }
+
+float sdfBlue(vec3 p){
+    float df = sdBox(vec3(p.xy, abs(p.z))- vec3(4.*b,0., b ), vec3(b, wall_height, wall_thickness )); // Edge 7 & 9
+    return df;
+}
+
 
 vec3 getNormal( vec3 p ) {
     const float eps = 0.0001;
@@ -172,65 +178,49 @@ void main() {
         if(h < eps ) {
             if( sdfBlack(pos) < eps)  //Edge 1 & 8
             {
-                pos.x += sign(pos.x) *(8. * b - eps);
+                pos.x -= sign(pos.x) *(8. * b - 4.*eps);
                 pos += h * ray;
                 collision_count += 1.;            
             } else if( sdfPink(pos) < eps) //Edge 2, 5, 10, 13
             {
-                // mat2 r = mat2(-1., 0.,
-                //               0., -1.); //wrong matrix for pink (correct for orange)
                 mat2 r = mat2(0., 1.,
                               -1., 0.);
                 pos.xz = r * (pos.xz - b*sign(pos.xz)) + b*sign(pos.xz);
                 ray.xz = r * ray.xz;
-                pos += (h+420.*eps) * ray;
+                pos += (h+n_eps*eps) * ray;
                 collision_count += 1.;  
             } else if( sdfGreen(pos) < eps) //Edge 3, 6, 11, 14
             {
-                // mat2 r = mat2(-1., 0.,
-                //               0., -1.); //wrong matrix for pink (correct for orange)
                 mat2 r = mat2(0., -1.,
                               1., 0.);
                 pos.xz = r * (pos.xz - b*sign(pos.xz)) + b*sign(pos.xz);
                 ray.xz = r * ray.xz;
-                pos += (h+420.*eps) * ray;
+                pos += (h+n_eps*eps) * ray;
                 collision_count += 1.;   
-             } else if( sdfOrange(pos) < eps) //Edge 4, 7, 9, 12
+            } else if( sdfOrange(pos) < eps) //Edge 4, 12
             {
-                pos.xz += sign(pos.x - 2.*b)*vec2(-4.*b, sign(pos.z)*2.*b);
+                pos.x *= -1.;
+                pos.xz += vec2(4.*b, -sign(pos.z)*2.*b);
+                ray.xz = -ray.xz;
+                pos += (h+n_eps*eps) * ray;
+                
+                
+                collision_count += 1.;  
+                // pos.xz += vec2(-4.*b, sign(pos.z)*2.*b);
+                // ray.xz =  -ray.xz;
+                // pos += (h+n_eps*eps) * ray;
+                // collision_count += 100.;       
+            }else if( sdfBlue(pos) < eps) //Edge 7, 9
+            {
+                pos.xz += vec2(-4.*b, sign(pos.z)*2.*b);
+                pos.x *= -1.;
                 ray.xz = - ray.xz;
-                pos += (h+420.*eps) * ray;
-                collision_count += 1.;       
-            // } else if( sdBox(pos - vec3(0., 0., -2.*b - eps), vec3( b, wall_height , eps )) < eps) //Edge 1
-            // {
-            //     pos.z = 2. * b - 2.*eps;
-            //     pos += h * ray;
-            //     collision_count += 1.;            
-            // }else if(sdBox(pos - vec3(2. * b, 0., -2.*b - eps), vec3( b, wall_height , eps )) < eps) // Edge 2
-            // {
-            //     pos.z = - 2.*eps;
-            //     pos += h * ray;
-            //     collision_count += 1.;            
-            // }else if( sdBox(pos- vec3(3.*b + eps, 0., -b), vec3(eps, wall_height, b )) < eps) //Edge 3
-            // {
-            //     pos.x = -b + 2.*eps;
-            //     pos += h * ray;
-            //     collision_count += 1.;            
-            // }else if( sdBox(pos- vec3(b + eps, 0. ,b), vec3(eps, wall_height, b )) < eps) //Edge 5
-            // {
-            //     pos.x = -b + 2.*eps;
-            //     pos += h * ray;
-            //     collision_count += 1.;            
-            // }else if( sdBox(pos- vec3(-b-eps ,0., b), vec3(eps, wall_height, 1.*b )) < eps) //Edge 7
-            // {
-            //     pos.x = b - 2.*eps;
-            //     pos += h * ray;
-            //     collision_count += 1.;
-            // }else if( sdBox(pos- vec3(-b - eps ,0., -b ), vec3(eps, wall_height, 1.*b )) < eps) //Edge 8
-            // {
-            //     pos.x = 3. * b - 2.*eps;
-            //     pos += h * ray;
-            //     collision_count += 1.;            
+                pos += (h+n_eps*eps) * ray;
+                collision_count += 1.;  
+                // pos.xz -= vec2(-4.*b, sign(pos.z)*2.*b);
+                // // ray.xz =  - ray.xz;
+                // pos += (h+n_eps*eps) * ray;
+                // collision_count += 1.;       
             }else if(t > tMax){ 
                 pos = vec3(tMax);
                 break;
